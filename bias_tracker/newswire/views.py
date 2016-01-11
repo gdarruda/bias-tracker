8d81432cb4e8c1081ea22103df1edf41bbff0465
Gabriel Arruda <gda.gabriel@gmail.com>
2016-01-10 23:39:02 -0200

    Adding button to add content

--- bias_tracker/newswire/views.py
+++ bias_tracker/newswire/views.py
@@ -1,5 +1,12 @@
+# coding=utf-8
+
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    profiles = [dict(id="@estadaopolitica", name="Política Estadão",
+                     description="Twitter oficial da editoria de Política do Estadão. Conheça também nossa página no Facebook"),
+                dict(id="@folha_poder", name="Folha Poder",
+                     description="Canal de diálogo com o leitor da editoria Poder do jornal Folha de S.Paulo")]
+
+    return render(request, 'index.html', {"profiles": profiles})
